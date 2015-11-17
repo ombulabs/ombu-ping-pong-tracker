6713e741d214bad3584c79bff5b4a87966e6e3bb
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-11-17 19:52:41 -0300

    Added @players

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ApplicationController
   def index
     @matches = Match.joins(:challenger, :opponent).order(played_at: :desc)
+    @players = Player.all
     @top_challenger, @max_challenges = Match.top_challenger
     @top_pair, @top_pair_played = Match.most_played_against_each_other
   end
